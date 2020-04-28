6a90e61854ef0cb206fc27c1cebeb952d89b5376
Akshay Chaudhary <64261194+alexith@users.noreply.github.com>
2020-04-29 02:36:25 +0530

    Add files via upload

---
+++ myhello.java
@@ -0,0 +1,31 @@
+public class myhello {
+    public static void main(String[] arg){
+        System.out.println("Hello, World!");                 //print function
+
+        int a;
+        int b;
+        int c;
+
+        a = 7;
+        b = 6;
+
+        c = 7 - 6;
+
+        System.out.println(c);
+
+        //this is a single line comment
+
+        /*this is multi-line comments, also char is a letter in single quotes, look for the line of code below,
+        btw my name is Akshay it's really nice to meet you :)*/
+
+        char myFirstNameInitial = 'A';
+
+        String mNameExceptInitial = "kshay";
+
+        //concatenation seen in below as we add to strings
+
+        System.out.println("My Name is:" + " " + myFirstNameInitial + mNameExceptInitial + " !");
+
+
+    }
+}

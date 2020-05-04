71c6bb6bd8453b840032ee8caff913559a054567
Akshay Chaudhary <64261194+alexith@users.noreply.github.com>
2020-05-04 18:30:30 +0530

    Add files via upload
    Brain storm code snippet #1
    here i thought that what if i try to use math.random()'s random() with an array[] of char'',
    it went just as i expected but i only got only one eror while using eclipse on fedora so thats a great improvement in learning java.

---
+++ myJava.java
@@ -0,0 +1,12 @@
+package myJava;
+
+public class myJava {
+	public static void main(String[] args) {
+		char[] myList = {'a', 'c', 'b', 'd'};
+		int myListLength = myList.length;
+		
+String shuffler = myList.random() * myListLength;
+		System.out.println(shuffler);
+	}
+}
+

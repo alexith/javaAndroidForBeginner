c2a319066af43165509226b6e932b17558fa0c88
Akshay Chaudhary <64261194+alexith@users.noreply.github.com>
2020-05-12 14:45:10 +0530

    Update whileLooping.java

--- whileLooping.java
+++ whileLooping.java
@@ -1,10 +1,15 @@
 public class whileLooping {
-    public static void main (String[] args){
-     int elevateMyNumbers = 0;
+    public static void main(String[] args) {
 
-     while(elevateMyNumbers <= 11){
+     int elevateMyNumbers = 0 ;
+
+        while(elevateMyNumbers <= 10) {
          System.out.println(elevateMyNumbers);
-         elevateMyNumbers += 1;
+
+         elevateMyNumbers += 2;
      }
+        for(elevateMyNumbers = 10; elevateMyNumbers >= 1; elevateMyNumbers--) {
+            System.out.println(elevateMyNumbers * 2);
+        }
     }
 }

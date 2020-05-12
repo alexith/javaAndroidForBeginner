4499550d50374cc2143e8d1e66c9c76ef0939012
Akshay Chaudhary <64261194+alexith@users.noreply.github.com>
2020-05-12 18:12:42 +0530

    Add files via upload

---
+++ familyLoop.java
@@ -0,0 +1,9 @@
+public class familyLoop {
+    public static void main(String[] args) {
+        String[] familyList = {"mummy", "papa", "me", "didi"};
+
+        for(int i = 0; i < familyList.length; i++){
+            System.out.println(familyList[i]);
+        }
+    }
+}

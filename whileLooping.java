41ffaf2dc45dfe0586a083af6807d89dfb930f0b
Akshay Chaudhary <64261194+alexith@users.noreply.github.com>
2020-05-12 14:25:24 +0530

    Add files via upload

---
+++ whileLooping.java
@@ -0,0 +1,10 @@
+public class whileLooping {
+    public static void main (String[] args){
+     int elevateMyNumbers = 0;
+
+     while(elevateMyNumbers <= 11){
+         System.out.println(elevateMyNumbers);
+         elevateMyNumbers += 1;
+     }
+    }
+}

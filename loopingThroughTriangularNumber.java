600767700ca8091c2c9f00e48c5d912cc6dc4d4a
Akshay Chaudhary <64261194+alexith@users.noreply.github.com>
2020-05-12 16:54:24 +0530

    Add files via upload

---
+++ loopingThroughTriangularNumber.java
@@ -0,0 +1,16 @@
+public class whileLooping {
+    public static void main(String[] args) {
+
+        int i = 1;
+        int triangularNumber = 1;
+
+        while(i<=10) {
+            System.out.println(triangularNumber);
+
+            i++;
+
+            triangularNumber = triangularNumber + i;
+
+        }
+    }
+}

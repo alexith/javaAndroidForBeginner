a2fd52576f2bbc07d93682872ff4f088afcd086c
Akshay Chaudhary <64261194+alexith@users.noreply.github.com>
2020-05-13 15:00:17 +0530

    Add files via upload

---
+++ arrayListFamilyLoop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class arrayListFamilyLoop {
+    public static void main(String[] args) {
+        List familyMemberList = new ArrayList();
+
+        familyMemberList.add("Mummy");
+        familyMemberList.add("papa");
+        familyMemberList.add("me");
+        familyMemberList.add("didi");
+
+        System.out.println(familyMemberList);
+    }
+}

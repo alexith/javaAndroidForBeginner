0315bcb13bb12fbbf069c8dbacda1a3791203221
Akshay Chaudhary <64261194+alexith@users.noreply.github.com>
2020-05-13 14:27:52 +0530

    Update familyLoop.java
    added alternate loop through list option for more understanding

--- familyLoop.java
+++ familyLoop.java
@@ -5,5 +5,11 @@ public class familyLoop {
         for(int i = 0; i < familyList.length; i++){
             System.out.println(familyList[i]);
         }
+
+        //Another way but less customization option given below in case
+
+        for(String name : familyList){
+            System.out.println(name);
+        }
     }
 }
